1231c2ae6cffcb7502bb85fbed7619941d00c600
Willder Canabrava <willcanabrava@gmail.com>
2023-07-11 10:34:16 -0300

    Adicionando mensagem de finalização.

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Tabuada {
+import java.util.Scanner;
+
+public class Tabuada {
+
+    public static void main(String[] args) {
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um número de 1 a 10 para ver sua tabuada: ");
+        int numero = leitura.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero*i));
+            System.out.println("Fim!");
+
+        }
+    }
 }
